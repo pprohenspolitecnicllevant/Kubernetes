518abd9921ec7e68338875d9fd44123785d07253
logangr <145839068+pprohenspolitecnicllevant@users.noreply.github.com>
2024-03-25 17:37:05 +0100

    servei web ok

--- Final/README.md
+++
@@ -1,8 +0,0 @@
-Exercici1
-kind create cluster --name prohens --config .\cluster.yml
-kubectl apply -f .\ns.yml
-kubectl apply -f .\web-deployment.yml
-
-falta liveness readness i envs per connectar base de dades.
-
-Exercici2
--- Final/ejercicio1/ejercicio1.sh
+++ Final/ejercicio1/ejercicio1.sh
@@ -6,5 +6,20 @@ kind create cluster --name prohens --config ./cluster.yml
 # Cream els dos namespace
 kubectl apply -f ./ns.yml
 
+# Cream ingress controller
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s
+
+# Creem el configmap
+kubectl apply -f ./configmap.yml
+
 # Deploy del sistema web
 kubectl apply -f ./web-deployment.yml
+kubectl apply -f ./content-deployment.yml
+
+# Services
+kubectl apply -f ./service-web.yml
+kubectl apply -f ./service-content.yml
+
+# Service
+kubectl apply -f ./ingress.yml
